248df512766f20bde7ff44e22af89b6e749d0f5e
Herrerasaurus <tetecookie@gmail.com>
2024-10-19 00:10:52 +0000

    Adding canvas and effects - step 1

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,15 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+// add app title to webpage
+/*const appTitle = document.createElement("h1");
+appTitle.innerHTML = APP_NAME;
+*/
+//add a canvas to the webpage (size 256x256)
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+
+//document.body.append(appTitle);
+document.body.append(canvas);
