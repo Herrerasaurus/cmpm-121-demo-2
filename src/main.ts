f7f8688598c9b247071846c2cc0542152b17781e
itzzbeatrizz <100254009+itzzbeatrizz@users.noreply.github.com>
2024-11-20 19:04:47 -0800

    Refactoring button creation into a helper function

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import "./style.css";
 
 const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
-
 document.title = APP_NAME;
 
 // add app title to webpage
@@ -10,33 +9,28 @@ const appTitle = document.createElement("h1");
 appTitle.innerHTML = APP_NAME;
 app.append(appTitle);
 
-
 //adding export feature
-const exportButton = document.createElement("button");
-exportButton.innerHTML = "Export";
-exportButton.addEventListener("click", () => {
+// Refectoring export button with helper function -- itzzbeatrizz
+app.append(createButton("Export", () => {
     //temporary canvas object of 1024x1024 size
     const exportCanvas = document.createElement("canvas");
     exportCanvas.width = 1024;
     exportCanvas.height = 1024;
-    
+
     //prepare canvasrenderincontext2d object for canvas, using scale (x,y)
     ctx = exportCanvas.getContext("2d");
-    if(ctx){
+    if (ctx) {
         ctx.scale(4, 4);
-        commands.forEach(command => command.display());
+        commands.forEach((command) => command.display());
         ctx.scale(1, 1);
         ctx = canvas.getContext("2d");
-
     }
 
     const anchor = document.createElement("a");
     anchor.href = exportCanvas.toDataURL("image/png");
     anchor.download = "sketchpad.png";
     anchor.click();
-
-});
-app.append(exportButton);
+}));
 app.append(document.createElement("br"));
 app.append(document.createElement("br"));
 
@@ -63,8 +57,8 @@ canvas.addEventListener("drawing-changed", () => {
     // clear canvas
     if (ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        commands.forEach(command => command.display());
-        if(cursorCommand){
+        commands.forEach((command) => command.display());
+        if (cursorCommand) {
             cursorCommand.display(ctx);
         }
     }
@@ -73,14 +67,21 @@ canvas.addEventListener("drawing-changed", () => {
 let cursor = "*";
 let lineColor = getRandomColor();
 
-class CursorCommand{
+class CursorCommand {
     cursor: string;
     x: number;
     y: number;
     lineWidth: number;
     lineColor: string;
     rotationValue: number;
-    constructor (cursor: string, x: number, y: number, lineWidth: number, lineColor: string, rotationValue: number){
+    constructor(
+        cursor: string,
+        x: number,
+        y: number,
+        lineWidth: number,
+        lineColor: string,
+        rotationValue: number,
+    ) {
         this.cursor = cursor;
         this.x = x;
         this.y = y;
@@ -88,50 +89,46 @@ class CursorCommand{
         this.lineColor = lineColor;
         this.rotationValue = rotationValue;
     }
-    display(ctx: CanvasRenderingContext2D){
-            let xShift = 0;
-            let yShift = 0;            
-            ctx.fillStyle = this.lineColor;
-            if(this.lineWidth == 6){
-                xShift = 10;
-                yShift = 18;
-                ctx.font = "40px Arial";
-                ctx.fillText(this.cursor, this.x - xShift, this.y + yShift);
-
-            }else if (this.lineWidth == 3){
-                xShift = 8;
-                yShift = 12;
-                ctx.font = "30px Arial";
-                ctx.fillText(this.cursor, this.x - xShift, this.y + yShift);
-
-            }else{
-                xShift = 15;
-                yShift = 6;
-                ctx.font = "30px Arial";
-                ctx.save();
-                const textMetrics = ctx.measureText(cursor);
-                const width = textMetrics.width;
-                const height = 30;
-                ctx.translate(this.x + width/2, this.y + height / 2);
-                ctx.rotate(rotationValue * Math.PI / 180);
-                ctx.fillText(cursor, -width/2, height / 2);
-                ctx.restore();
-            }
+    display(ctx: CanvasRenderingContext2D) {
+        let xShift = 0;
+        let yShift = 0;
+        ctx.fillStyle = this.lineColor;
+        if (this.lineWidth == 6) {
+            xShift = 10;
+            yShift = 18;
+            ctx.font = "40px Arial";
+            ctx.fillText(this.cursor, this.x - xShift, this.y + yShift);
+        } else if (this.lineWidth == 3) {
+            xShift = 8;
+            yShift = 12;
+            ctx.font = "30px Arial";
+            ctx.fillText(this.cursor, this.x - xShift, this.y + yShift);
+        } else { // Updating cursor for stickers -- itzzbeatrizz
+            ctx.font = "30px Arial";
+            ctx.save();
+            const textMetrics = ctx.measureText(cursor);
+            const width = textMetrics.width;
+            const height = 30;
+            ctx.translate(this.x + width / 2, this.y + height / 2);
+            ctx.rotate(rotationValue * Math.PI / 180);
+            ctx.fillText(cursor, -width / 2, height / 2);
+            ctx.restore();
         }
     }
+}
 
-class LineCommand{
-    points: {x: number; y: number; lineWidth: number; lineColor: string;}[];
-    constructor (x: number, y: number, lineWidth: number, lineColor: string){
-        this.points = [{x, y, lineWidth, lineColor}];
+class LineCommand {
+    points: { x: number; y: number; lineWidth: number; lineColor: string }[];
+    constructor(x: number, y: number, lineWidth: number, lineColor: string) {
+        this.points = [{ x, y, lineWidth, lineColor }];
     }
-    display(){
+    display() {
         if (ctx) {
             ctx.strokeStyle = this.points[0].lineColor;
             ctx.beginPath();
-            const {x, y} = this.points[0];
+            const { x, y } = this.points[0];
             ctx.moveTo(x, y);
-            for(const {x, y, lineWidth, lineColor} of this.points){
+            for (const { x, y, lineWidth, lineColor } of this.points) {
                 ctx.lineWidth = lineWidth;
                 ctx.strokeStyle = lineColor;
                 ctx.lineTo(x, y);
@@ -139,35 +136,35 @@ class LineCommand{
             ctx.stroke();
         }
     }
-    grow(x: number, y: number, lineWidth: number, lineColor: string){
-        this.points.push({x, y, lineWidth, lineColor});
+    grow(x: number, y: number, lineWidth: number, lineColor: string) {
+        this.points.push({ x, y, lineWidth, lineColor });
     }
 }
 
 let rotationValue = 0;
 
-class StickerCommand{
-    points: {x: number; y: number; cursor: string, rotationValue: number}[];
-    constructor (x: number, y: number, cursor: string, rotationValue: number){
-        this.points = [{x, y, cursor, rotationValue}];
+class StickerCommand {
+    points: { x: number; y: number; cursor: string; rotationValue: number }[];
+    constructor(x: number, y: number, cursor: string, rotationValue: number) {
+        this.points = [{ x, y, cursor, rotationValue }];
     }
-    display(){
+    display() {
         if (ctx) {
             //place one of the stickers
             ctx.font = "30px Arial";
-            const {x, y, cursor, rotationValue} = this.points[0];
+            const { x, y, cursor, rotationValue } = this.points[0];
             ctx.save();
             const textMetrics = ctx.measureText(cursor);
             const width = textMetrics.width;
             const height = 30;
-            ctx.translate(x + width/2, y + height / 2);
+            ctx.translate(x + width / 2, y + height / 2);
             ctx.rotate(rotationValue * Math.PI / 180);
-            ctx.fillText(cursor, -width/2, height / 2);
+            ctx.fillText(cursor, -width / 2, height / 2);
             ctx.restore();
         }
     }
-    grow(x: number, y: number, cursor: string, rotationValue: number){
-        this.points.push({x, y, cursor, rotationValue});
+    grow(x: number, y: number, cursor: string, rotationValue: number) {
+        this.points.push({ x, y, cursor, rotationValue });
     }
 }
 
@@ -182,40 +179,69 @@ canvas.addEventListener("mouseout", () => {
 });
 
 canvas.addEventListener("mouseenter", (e) => {
-    cursorCommand = new CursorCommand(cursor, e.offsetX, e.offsetY, lineWidth, lineColor, rotationValue);
+    cursorCommand = new CursorCommand(
+        cursor,
+        e.offsetX,
+        e.offsetY,
+        lineWidth,
+        lineColor,
+        rotationValue,
+    );
     canvas.dispatchEvent(updateCanvas);
 });
 
 // get user input for drawing
 canvas.addEventListener("mousedown", (e) => {
-    if(cursor == "*"){
-        currentLineCommand = new LineCommand(e.offsetX, e.offsetY, lineWidth, lineColor);
-    }else{
-        currentLineCommand = new StickerCommand(e.offsetX, e.offsetY, cursor, rotationValue);
+    // Refactoring code -- itzzbeatrizz
+    cursor === "*" ? lineColor = getRandomColor() : lineColor = cursor;
+    if (cursor == "*") {
+        currentLineCommand = new LineCommand(
+            e.offsetX,
+            e.offsetY,
+            lineWidth,
+            lineColor,
+        );
+    } else {
+        currentLineCommand = new StickerCommand(
+            e.offsetX,
+            e.offsetY,
+            cursor,
+            rotationValue,
+        );
     }
+
     commands.push(currentLineCommand);
     redoCommands.splice(0, redoCommands.length);
     canvas.dispatchEvent(updateCanvas);
 });
 
-
 canvas.addEventListener("mousemove", (e) => {
-    cursorCommand = new CursorCommand(cursor, e.offsetX, e.offsetY, lineWidth, lineColor, rotationValue);
+    cursorCommand = new CursorCommand(
+        cursor,
+        e.offsetX,
+        e.offsetY,
+        lineWidth,
+        lineColor,
+        rotationValue,
+    );
     canvas.dispatchEvent(updateCanvas);
-    if(cursor == "*"){
+    if (cursor == "*") {
         if (currentLineCommand instanceof LineCommand) {
-            currentLineCommand.points.push({x: e.offsetX, y: e.offsetY, lineWidth, lineColor});
+            currentLineCommand.points.push({
+                x: e.offsetX,
+                y: e.offsetY,
+                lineWidth,
+                lineColor,
+            });
         }
-    }else{
-        if(currentLineCommand instanceof StickerCommand){
+    } else {
+        if (currentLineCommand instanceof StickerCommand) {
             //transform sticker position
             currentLineCommand.points[0].x = e.offsetX;
             currentLineCommand.points[0].y = e.offsetY;
         }
-
     }
     canvas.dispatchEvent(updateCanvas);
-
 });
 
 canvas.addEventListener("mouseup", () => {
@@ -223,22 +249,16 @@ canvas.addEventListener("mouseup", () => {
     canvas.dispatchEvent(updateCanvas);
 });
 
-// add clear button
-const clearButton = document.createElement("button");
-clearButton.innerHTML = "Clear";
-clearButton.addEventListener("click", () => {
+// Refactoring clear, undo, and redo buttons with helper function -- itzzbeatrizz
+app.append(createButton("Clear", () => {
     if (ctx) {
         commands.splice(0, commands.length);
         redoCommands.splice(0, redoCommands.length);
         canvas.dispatchEvent(updateCanvas);
     }
-});
-app.append(clearButton);
+}));
 
-// add undo button
-const undoButton = document.createElement("button");
-undoButton.innerHTML = "Undo";
-undoButton.addEventListener("click", () => {
+app.append(createButton("Undo", () => {
     if (ctx && commands.length > 0) {
         const command = commands.pop();
         if (command) {
@@ -246,13 +266,9 @@ undoButton.addEventListener("click", () => {
         }
         canvas.dispatchEvent(updateCanvas);
     }
-});
-app.append(undoButton);
+}));
 
-// add redo button
-const redoButton = document.createElement("button");
-redoButton.innerHTML = "Redo";
-redoButton.addEventListener("click", () => {
+app.append(createButton("Redo", () => {
     if (ctx && redoCommands.length > 0) {
         const command = redoCommands.pop();
         if (command) {
@@ -260,39 +276,29 @@ redoButton.addEventListener("click", () => {
         }
         canvas.dispatchEvent(updateCanvas);
     }
-});
-app.append(redoButton);
+}));
+
 app.append(document.createElement("br"));
 
 //randomize color
-function getRandomColor(){
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for(let i = 0; i < 6; i++){
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function getRandomColor() {
+    // Simplified random color generator -- itzzbeatrizz
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 //adding different line width buttons
-const thickLine = document.createElement("button");
-thickLine.innerHTML = "Thick Line";
-thickLine.addEventListener("click", () => {
+// Refactoring line width buttons with helper function -- itzzbeatrizz
+app.append(createButton("Thick Line", () => {
     lineColor = getRandomColor();
     lineWidth = 6;
     cursor = "*";
-});
+}));
 
-const thinLine = document.createElement("button");
-thinLine.innerHTML = "Thin Line";
-thinLine.addEventListener("click", () => {
+app.append(createButton("Thin Line", () => {
     lineColor = getRandomColor();
     lineWidth = 3;
     cursor = "*";
-});
-
-app.append(thickLine);
-app.append(thinLine);
+}));
 app.append(document.createElement("br"));
 
 // rotate sticker orientation with range slider
@@ -309,7 +315,6 @@ rotateSlider.addEventListener("input", (e) => {
     canvas.dispatchEvent(updateCanvas);
 });
 
-
 const rotateLabel = document.createElement("label");
 rotateLabel.innerHTML = "Rotate Sticker";
 
@@ -318,38 +323,38 @@ app.append(document.createElement("br"));
 app.append(rotateSlider);
 app.append(document.createElement("br"));
 
-interface Sticker{
-    emoji: string,
-};
+interface Sticker {
+    emoji: string;
+}
 
 const stickers: Sticker[] = [
-    {emoji: "{custom}"},
-    {emoji: "🧌"},
-    {emoji: "🦆"},
-    {emoji: "😜"}
+    { emoji: "{custom}" },
+    { emoji: "🧌" },
+    { emoji: "🦆" },
+    { emoji: "😜" },
 ];
 
 // function, add button
-class addButton{
+class addButton {
     emoji: string;
-    constructor(emoji: string){
+    constructor(emoji: string) {
         this.emoji = emoji;
     }
-    display(){
+    display() {
         const button = document.createElement("button");
         button.innerHTML = this.emoji;
         button.addEventListener("click", () => {
             lineWidth = 0;
-            if(this.emoji == "{custom}"){
+            if (this.emoji == "{custom}") {
                 const customSticker = prompt("Enter a custom sticker", "😀");
-                if(customSticker != null){
-                    const newSticker = {emoji: customSticker};
+                if (customSticker != null) {
+                    const newSticker = { emoji: customSticker };
                     stickers.push(newSticker);
                     const newButton = new addButton(customSticker);
                     newButton.display();
                     cursor = customSticker;
                 }
-            }else{
+            } else {
                 cursor = this.emoji;
             }
             canvas.dispatchEvent(updateCanvas);
@@ -358,18 +363,16 @@ class addButton{
     }
 }
 
-for(let i = 0; i < stickers.length; i++){
+for (let i = 0; i < stickers.length; i++) {
     const sticker = stickers[i];
     const newButton = new addButton(sticker.emoji);
     newButton.display();
 }
 
-
-
-
-
-
-
-
-
-
+// Refactoring button creation into a helper function -- itzzbeatrizz
+function createButton(text: string, onClick: () => void) {
+    const button = document.createElement("button");
+    button.innerHTML = text;
+    button.addEventListener("click", onClick);
+    return button;
+}
